a90b9aaf48b811cfdab91f3600d4c86a834eb5a8
PRANSI GUPTA <pransigupta2686@gmail.com>
2026-07-06 22:56:44 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,12 @@
+#calc.py
+#import calc
+#def add(x,y):
+    #return(x+y)
+#def  sub(x,y):
+   # return(x-y)
+#print(calc.add(10,2))
+
+#Import from Module
+from math import sqrt,factorial
+print(sqrt(16))
+print(factorial(16))
